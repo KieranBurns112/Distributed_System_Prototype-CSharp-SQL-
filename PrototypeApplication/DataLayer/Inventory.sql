cdc0b47091561302c03e585d6388ef53b3e78f0b
KieranBurns112 <kieran.burns@yahoo.com>
2019-11-03 19:07:50 +0000

    Begun Prototype Development

---
+++ PrototypeApplication/DataLayer/Inventory.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[ItemId] INT NOT NULL PRIMARY KEY, 
+    [ItemName] NVARCHAR(50) NOT NULL, 
+    [ItemPrice] DECIMAL(6, 2) NOT NULL, 
+    [ItemStock] DECIMAL(6) NULL, 
+    [StandardSpecialOffer] INT NULL, 
+    [LoyaltySpecialOffer] INT NULL,
+)
---
+++ PrototypeApplication/PresentationLayer/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PresentationLayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PriceControlNavButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InventoryControlNavButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoyaltyCardNavButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FinanceApprovalNavButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReportsAndAnalysisNavButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
